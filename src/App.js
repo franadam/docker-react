737160537c916b2342e4f241cd6302da36ccf3d4
franadam <francoisadam@hotmail.be>
2022-02-28 22:12:13 +0100

    feat add text

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Bye My Love</p>
+        <p>I'm the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
